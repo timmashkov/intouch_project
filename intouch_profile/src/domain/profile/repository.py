6adfed70ca26af6eafd121e5c97b9ca8e1883bd4
timmashkov <bodomizer@yandex.ru>
2024-03-24 12:01:05 +0300

    added service layer following cqrs pattern

--- intouch_profile/src/domain/profile/repository.py
+++ intouch_profile/src/domain/profile/repository.py
@@ -1,8 +1,14 @@
 from fastapi import Depends
-from sqlalchemy import select
+from sqlalchemy import select, insert, update, delete
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from domain.profile.schema import GetProfileById
+from intouch_profile.src.domain.profile.schema import (
+    GetProfileById,
+    ProfileReturn,
+    GetProfileByFirstName,
+    GetProfileByLastName,
+    CreateUpdateProfile,
+)
 from intouch_profile.src.infrastructure.database.models import Profile
 from intouch_profile.src.infrastructure.database.connector import tempest
 
@@ -18,5 +24,93 @@ class ProfileShowRepository:
         result = list(answer.scalars().all())
         return result
 
-    async def get_profile_by_id(self, cmd: GetProfileById):
-        pass
+    async def get_profile_by_id(self, cmd: GetProfileById) -> ProfileReturn | None:
+        stmt = select(self.model).where(self.model.id == cmd.id)
+        answer = await self.session.execute(stmt)
+        result = answer.scalar_one_or_none()
+        return result
+
+    async def get_profile_by_first_name(
+        self, cmd: GetProfileByFirstName
+    ) -> ProfileReturn | None:
+        stmt = select(self.model).where(self.model.id == cmd.first_name)
+        answer = await self.session.execute(stmt)
+        result = answer.scalar_one_or_none()
+        return result
+
+    async def get_profile_by_last_name(
+        self, cmd: GetProfileByLastName
+    ) -> ProfileReturn | None:
+        stmt = select(self.model).where(self.model.id == cmd.last_name)
+        answer = await self.session.execute(stmt)
+        result = answer.scalar_one_or_none()
+        return result
+
+
+class ProfileDataManagerRepository:
+    def __init__(self, session: AsyncSession = Depends(tempest.enter_session)):
+        self.session = session
+        self.model = Profile
+
+    async def create_profile(self, cmd: CreateUpdateProfile) -> ProfileReturn | None:
+        stmt = (
+            insert(self.model)
+            .values(**cmd.model_dump())
+            .returning(
+                self.model.id,
+                self.model.first_name,
+                self.model.last_name,
+                self.model.occupation,
+                self.model.bio,
+                self.model.status,
+                self.model.created_at,
+                self.model.is_active,
+            )
+        )
+        answer = await self.session.execute(stmt)
+        await self.session.commit()
+        result = answer.mappings().first()
+        return result
+
+    async def update_profile(
+        self, cmd: CreateUpdateProfile, data: GetProfileById
+    ) -> ProfileReturn | None:
+        stmt = (
+            update(self.model)
+            .where(self.model.id == data.id)
+            .values(**cmd.model_dump())
+            .returning(
+                self.model.id,
+                self.model.first_name,
+                self.model.last_name,
+                self.model.occupation,
+                self.model.bio,
+                self.model.status,
+                self.model.created_at,
+                self.model.is_active,
+            )
+        )
+        answer = await self.session.execute(stmt)
+        await self.session.commit()
+        result = answer.mappings().first()
+        return result
+
+    async def delete_profile(self, cmd: GetProfileById) -> ProfileReturn | None:
+        stmt = (
+            delete(self.model)
+            .where(self.model.id == cmd.id)
+            .returning(
+                self.model.id,
+                self.model.first_name,
+                self.model.last_name,
+                self.model.occupation,
+                self.model.bio,
+                self.model.status,
+                self.model.created_at,
+                self.model.is_active,
+            )
+        )
+        answer = await self.session.execute(stmt)
+        await self.session.commit()
+        result = answer.mappings().first()
+        return result
---
+++ intouch_profile/src/service/service.py
@@ -0,0 +1,66 @@
+from fastapi import Depends
+
+from intouch_profile.src.domain.profile.schema import (
+    ProfileReturn,
+    GetProfileById,
+    GetProfileByFirstName,
+    GetProfileByLastName,
+    CreateUpdateProfile,
+)
+from intouch_profile.src.domain.profile.repository import (
+    ProfileShowRepository,
+    ProfileDataManagerRepository,
+)
+
+from intouch_profile.src.infrastructure.database.models import Profile
+
+
+class ProfileShowService:
+    def __init__(
+        self, repository: ProfileShowRepository = Depends(ProfileShowRepository)
+    ):
+        self.repository = repository
+
+    async def get_profiles(self) -> list[Profile]:
+        answer = await self.repository.get_profiles()
+        return answer
+
+    async def get_profile_by_id(self, cmd: GetProfileById) -> ProfileReturn:
+        answer = await self.repository.get_profile_by_id(cmd=cmd)
+        return answer
+
+    async def get_profile_by_first_name(
+        self, cmd: GetProfileByFirstName
+    ) -> ProfileReturn:
+        answer = await self.repository.get_profile_by_first_name(cmd=cmd)
+        return answer
+
+    async def get_profile_by_last_name(
+        self, cmd: GetProfileByLastName
+    ) -> ProfileReturn:
+        answer = await self.repository.get_profile_by_last_name(cmd=cmd)
+        return answer
+
+
+class ProfileDataManagerService:
+    def __init__(
+        self,
+        repository: ProfileDataManagerRepository = Depends(
+            ProfileDataManagerRepository
+        ),
+    ):
+        self.repository = repository
+
+    async def create_profile(self, cmd: CreateUpdateProfile) -> ProfileReturn:
+        answer = await self.repository.create_profile(cmd=cmd)
+        return answer
+
+    async def update_profile(
+        self, cmd: CreateUpdateProfile, data: GetProfileById
+    ) -> ProfileReturn:
+        answer = await self.repository.update_profile(cmd=cmd, data=data)
+        return answer
+
+    async def delete_profile(self, cmd: GetProfileById) -> ProfileReturn:
+        answer = await self.repository.delete_profile(cmd=cmd)
+        return answer
